1b17ff5d372ac2859f20a8d7ee87391308ea9457
barancem4 <96594555+barancemes@users.noreply.github.com>
2022-04-28 17:29:37 +0300

    js todo app

---
+++ bce.js
@@ -0,0 +1,82 @@
+// alert('Baran cem')
+
+// console.log("Baran Cem");
+// console.log(16);
+// var x=5;
+// var y=7;
+// console.log(x+y);
+
+// let gun=20;
+// let ay=8;
+// let yil=2002;
+// console.log(gun,ay,yil);
+// console.log("Baran Cem "+gun+"/"+ay+"/"+yil+" tarihinde doğmuştur");
+// let değiştirilebilir.
+// var fiyati=100000;
+// console.log(fiyati);
+// const arabafiyati=350000;
+// console.log(arabafiyati);
+// const değiştirilemez
+
+
+const addForm=document.querySelector('.add');
+const list=document.querySelector('.todos');
+const search=document.querySelector('.search input');
+
+const generateTemplate = todo => {
+    const html = ` <li class="list-group-item d-flex justify-content-between align-items-center">
+            <span>${todo}</span>
+            <i class="bi bi-trash-fill delete"></i>
+        </li>
+    `;
+    list.innerHTML+=html;
+}
+
+addForm.addEventListener('submit',e=>{
+    e.preventDefault();
+    const todo=addForm.add.value.trim();
+    if (todo.length)
+    {
+        generateTemplate(todo);
+        addForm.reset();
+    }
+})
+
+list.addEventListener('click',e=>{
+    if (e.target.classList.contains('delete'))
+    {
+        e.target.parentElement.remove();
+    }
+})
+
+const filterTodos = term =>{
+    Array.from(list.children)
+        .filter(todo => !todo.textContent.toLowerCase().includes(term))
+        .forEach(todo => todo.classList.add('filtered'));
+
+    Array.from(list.children)
+        .filter(todo => todo.textContent.toLowerCase().includes(term))
+        .forEach(todo => todo.classList.remove('filtered'));
+}
+
+search.addEventListener('keyup',()=>{
+    const term=search.value.trim().toLowerCase();
+    filterTodos(term);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
